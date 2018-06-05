c39fe21b372f7072fd3edd417723edec499dfd01
unrevre <unrevre@gmail.com>
2018-06-05 19:47:16 +0800

    qemu: minimal build for qemu
    disable unnecessary features

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -16,17 +16,9 @@ class Qemu < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
-  depends_on "gnutls"
-  depends_on "jpeg"
   depends_on "libpng"
-  depends_on "libssh"
-  depends_on "libusb"
-  depends_on "lzo"
   depends_on "ncurses"
-  depends_on "nettle"
   depends_on "pixman"
-  depends_on "snappy"
-  depends_on "vde"
 
   # 820KB floppy disk image file of FreeDOS 1.2, used to test QEMU
   resource "test-image" do
@@ -43,12 +35,42 @@ class Qemu < Formula
       --host-cc=#{ENV.cc}
       --disable-bsd-user
       --disable-guest-agent
+      --target-list=i386-softmmu,x86_64-softmmu
+      --audio-drv-list=
       --enable-curses
-      --enable-libssh
-      --enable-vde
       --extra-cflags=-DNCURSES_WIDECHAR=1
-      --disable-sdl
+      --disable-attr
+      --disable-brlapi
+      --disable-cap-ng
+      --disable-curl
+      --disable-fdt
+      --disable-gcrypt
+      --disable-glusterfs
+      --disable-gnutls
       --disable-gtk
+      --disable-libiscsi
+      --disable-libssh
+      --disable-linux-aio
+      --disable-live-block-migration
+      --disable-lzo
+      --disable-nettle
+      --disable-opengl
+      --disable-qom-cast-debug
+      --disable-rbd
+      --disable-replication
+      --disable-sdl
+      --disable-seccomp
+      --disable-snappy
+      --disable-tpm
+      --disable-vde
+      --disable-vhost-net
+      --disable-vhost-scsi
+      --disable-vhost-user
+      --disable-vhost-vsock
+      --disable-virglrenderer
+      --disable-virtfs
+      --disable-vnc
+      --disable-xfsctl
     ]
     # Sharing Samba directories in QEMU requires the samba.org smbd which is
     # incompatible with the macOS-provided version. This will lead to
@@ -67,38 +89,8 @@ class Qemu < Formula
 
   test do
     expected = build.stable? ? version.to_s : "QEMU Project"
-    assert_match expected, shell_output("#{bin}/qemu-system-aarch64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-alpha --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-arm --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-cris --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-hppa --version")
     assert_match expected, shell_output("#{bin}/qemu-system-i386 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-lm32 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-m68k --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-microblaze --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-microblazeel --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mips --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mips64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mips64el --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mipsel --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-moxie --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-nios2 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-or1k --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-ppc --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-ppc64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-riscv32 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-riscv64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-rx --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-s390x --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sh4 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sh4eb --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sparc --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sparc64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-tricore --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-unicore32 --version")
     assert_match expected, shell_output("#{bin}/qemu-system-x86_64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-xtensa --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-xtensaeb --version")
     resource("test-image").stage testpath
     assert_match "file format: raw", shell_output("#{bin}/qemu-img info FLOPPY.img")
   end
