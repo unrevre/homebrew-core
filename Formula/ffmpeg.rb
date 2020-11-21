35e7889e78b94bc963b5e703e47d8c7efeb41a36
unrevre <unrevre@gmail.com>
2020-11-21 14:19:21 -0700

    ffmpeg: minimal build for personal use
    limited support for audio/video formats and codecs; support centred
    around processing of downloaded videos.

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -33,87 +33,44 @@ class Ffmpeg < Formula
 
   depends_on "nasm" => :build
   depends_on "pkg-config" => :build
-  depends_on "aom"
-  depends_on "dav1d"
-  depends_on "fontconfig"
-  depends_on "freetype"
-  depends_on "frei0r"
-  depends_on "gnutls"
-  depends_on "lame"
-  depends_on "libass"
-  depends_on "libbluray"
-  depends_on "libsoxr"
-  depends_on "libvidstab"
-  depends_on "libvorbis"
-  depends_on "libvpx"
-  depends_on "opencore-amr"
-  depends_on "openjpeg"
   depends_on "opus"
-  depends_on "rav1e"
-  depends_on "rtmpdump"
-  depends_on "rubberband"
   depends_on "sdl2"
-  depends_on "snappy"
-  depends_on "speex"
-  depends_on "srt"
-  depends_on "tesseract"
-  depends_on "theora"
-  depends_on "webp"
-  depends_on "x264"
-  depends_on "x265"
-  depends_on "xvid"
   depends_on "xz"
 
   uses_from_macos "bzip2"
-  uses_from_macos "libxml2"
   uses_from_macos "zlib"
 
   def install
     args = %W[
       --prefix=#{prefix}
-      --enable-shared
-      --enable-pthreads
-      --enable-version3
-      --enable-avresample
       --cc=#{ENV.cc}
       --host-cflags=#{ENV.cflags}
       --host-ldflags=#{ENV.ldflags}
-      --enable-ffplay
-      --enable-gnutls
+      --disable-autodetect
+      --disable-everything
+      --disable-network
       --enable-gpl
-      --enable-libaom
-      --enable-libbluray
-      --enable-libdav1d
-      --enable-libmp3lame
+      --enable-nonfree
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-bsf=aac_adtstoasc
+      --enable-encoder=aac,mpeg4,libopus
+      --enable-decoder=aac,mpeg4,libopus
+      --enable-muxer=adts,flv,matroska,mp4,opus
+      --enable-demuxer=aac,concat,flv,live_flv,matroska,mov,ogg
+      --enable-parser=aac,opus
+      --enable-protocol=file,pipe
+      --enable-ffmpeg
+      --enable-ffplay
+      --enable-audiotoolbox
+      --enable-avfoundation
+      --enable-bzlib
       --enable-libopus
-      --enable-librav1e
-      --enable-librubberband
-      --enable-libsnappy
-      --enable-libsrt
-      --enable-libtesseract
-      --enable-libtheora
-      --enable-libvidstab
-      --enable-libvorbis
-      --enable-libvpx
-      --enable-libwebp
-      --enable-libx264
-      --enable-libx265
-      --enable-libxml2
-      --enable-libxvid
       --enable-lzma
-      --enable-libfontconfig
-      --enable-libfreetype
-      --enable-frei0r
-      --enable-libass
-      --enable-libopencore-amrnb
-      --enable-libopencore-amrwb
-      --enable-libopenjpeg
-      --enable-librtmp
-      --enable-libspeex
-      --enable-libsoxr
+      --enable-sdl2
       --enable-videotoolbox
-      --disable-libjack
-      --disable-indev=jack
+      --enable-zlib
     ]
 
     system "./configure", *args
